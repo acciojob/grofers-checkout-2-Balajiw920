50cc95e80fb1d7a7cb41be460297a18ca99ec0b9
Balajiw920 <wbalaji2021@gmail.com>
2023-05-28 16:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function calculateTotalPrice() {
+  const prices = document.querySelectorAll('[data-ns-test="prices"]');
+  let totalPrice = 0;
+
+  prices.forEach((price) => {
+    totalPrice += parseFloat(price.textContent);
+  });
+
+  const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+  grandTotalElement.textContent = totalPrice.toFixed(2);
+}
+
+calculateTotalPrice();
